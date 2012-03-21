dc84b42d9a566c3810e89c3695edf35bf0a8f9a5
Louis Mullie <louis.mullie@gmail.com>
2012-03-21 14:57:31 -0400

    Added dependency list and copyable module for documents.

---
+++ lib/treat/dependencies.rb
@@ -0,0 +1,25 @@
+class Treat::Dependencies
+  
+  Gem = [
+    
+    ['psych', '>= 1.2.2', '(un)serialize annotated entities to YAML format'],
+    ['nokogiri', '>= 1.4.0', 'read and parse XML and HTML formats'],
+    ['sdsykes-ferret', '>= 0.11.6.19', 'perform full-text search in collections'],
+    ['lda-ruby', '>= 0.3.8', 'extract topic words from documents and collections'],
+    ['ruby-readability', '>= 0.5.0', 'extract the readable content from HTML pages'],
+    ['stanford-core-nlp', '>= 0.1.8', 'tokenize, segment, parse texts and perform named entity recognition'],
+    ['whatlanguage', '>= 1.0.0', 'detect the language of text'],
+    ['linguistics', '>= 1.0.9', 'retrieve the inflection of nouns, verbs and numbers in English'],
+    ['punkt-segmenter', '>= 0.9.1', 'segment texts into sentences'],
+    ['chronic', '>= 0.6.7', 'detect date and time in text'],
+    ['decisiontree', '>= 0.3.0', 'perform decision tree classification of text entities']
+  ]
+  
+  Binary = [
+    ['ocropus', 'recognize text in image files'],
+    ['antiword', 'extract text from DOC files'],
+    ['poppler', 'extract text from PDF files'],
+    ['graphviz', 'export and visualize directed graphs']
+  ]
+
+end
---
+++ lib/treat/entities/abilities/copyable.rb
@@ -0,0 +1,44 @@
+module Treat::Entities::Abilities::Copyable
+  
+  require 'fileutils'
+  
+  def copy_into(collection)
+    unless collection.is_a?(
+    Treat::Entities::Collection)
+      raise Treat::Exception,
+      "Cannot copy an entity into " +
+      "something else than a collection."
+    end
+    if type == :document
+      copy_document_into(collection)
+    elsif type == :collection
+      copy_collection_into(collection)
+    else
+      raise Treat::Exception,
+      "Can only copy a document " +
+      "or collection into a collection."
+    end
+  end
+  
+  def copy_collection_into(collection)
+    copy = dup
+    f = File.dirname(folder)
+    f = f.split(File::SEPARATOR)[-1]
+    f = File.join(collection.folder, f)
+    FileUtils.mkdir(f) unless
+    FileTest.directory(f)
+    FileUtils.cp_r(folder, f)
+    copy.set :folder, f
+    copy
+  end
+  
+  def copy_document_into(collection)
+    copy = dup
+    f = File.basename(file)
+    f = File.join(collection.folder, f)
+    FileUtils.cp(file, f)
+    copy.set :file, f
+    copy
+  end
+
+end
