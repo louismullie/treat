21a3541ceaeed708edfcf62b072515042a4d40fd
Louis Mullie <louis.mullie@gmail.com>
2012-04-17 00:26:23 -0400

    Made Loaders a module across all files (removed inconsistency).

--- lib/treat/loaders/stanford.rb
+++ lib/treat/loaders/stanford.rb
@@ -1,37 +1,45 @@
-module Treat::Loaders::Stanford
+module Treat
   
-  require 'stanford-core-nlp'
-  
-  class << self
-    attr_accessor :jar_path
-    attr_accessor :model_path
-    attr_accessor :loaded
+  module Loaders
+
+    class Stanford
+
+      require 'stanford-core-nlp'
+
+      class << self
+        attr_accessor :jar_path
+        attr_accessor :model_path
+        attr_accessor :loaded
+      end
+
+      self.jar_path = Treat.bin + 'stanford/'
+      self.model_path = Treat.models + 'stanford/'
+      self.loaded = false
+
+      def self.load(language = nil)
+
+        return if self.loaded
+
+        language ||=
+        Treat::Languages.describe(
+        Treat.default_language)
+
+        StanfordCoreNLP.jar_path = self.jar_path
+        StanfordCoreNLP.model_path = self.model_path
+
+        StanfordCoreNLP.use(language)
+
+        StanfordCoreNLP.log_file =
+        NULL_DEVICE if Treat.silence
+
+        StanfordCoreNLP.bind
+
+        self.loaded = true
+
+      end
+
+    end
+    
   end
   
-  self.jar_path = Treat.bin + 'stanford/'
-  self.model_path = Treat.models + 'stanford/'
-  self.loaded = false
-  
-  def self.load(language = nil)
-    
-    return if self.loaded
-    
-    language ||= 
-    Treat::Languages.describe(
-    Treat.default_language)
-    
-    StanfordCoreNLP.jar_path = self.jar_path
-    StanfordCoreNLP.model_path = self.model_path
-
-    StanfordCoreNLP.use(language)
-    
-    StanfordCoreNLP.log_file =
-    NULL_DEVICE if Treat.silence
-    
-    StanfordCoreNLP.bind
-    
-    self.loaded = true
-    
-  end
-
 end
