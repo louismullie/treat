ad3c7a499bc3cf3ed0fd3a832b8e15538181d1ab
Louis Mullie <louis.mullie@gmail.com>
2012-04-20 18:24:49 -0400

    Version 1.0.3 - Final.

--- lib/treat.rb
+++ lib/treat.rb
@@ -10,7 +10,7 @@ module Treat
   end
 
   # The current version of Treat.
-  VERSION = "1.0.2"
+  VERSION = "1.0.3"
 
   # Add methods to handle syntactic sugar,
   # language configuration options, and paths.
