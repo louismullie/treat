c536643d4cad7ebb0ae0e0cc5cc12cf795dda7c8
Louis Mullie <louis.mullie@gmail.com>
2012-04-20 18:16:11 -0400

    Fixed typo in dependency version for ruby-readability.

--- lib/treat/dependencies.rb
+++ lib/treat/dependencies.rb
@@ -5,7 +5,7 @@ class Treat::Dependencies
     ['nokogiri', '>= 1.5.2', 'read and parse XML and HTML formats'],
     ['sdsykes-ferret', '>= 0.11.6.19', 'perform full-text search in collections'],
     ['lda-ruby', '>= 0.3.8', 'extract topic words from documents and collections'],
-    ['ruby-readability', '>= 0.5.2g', 'extract the readable content from HTML pages'],
+    ['ruby-readability', '>= 0.5.2', 'extract the readable content from HTML pages'],
     ['stanford-core-nlp', '>= 0.3.0', 'tokenize, segment, parse texts and perform named entity recognition'],
     ['whatlanguage', '>= 1.0.0', 'detect the language of text'],
     ['linguistics', '>= 1.0.9', 'retrieve the inflection of nouns, verbs and numbers in English'],
