a9cbe2c3c26ade118e33bcd93b66d1007cccc7fe
Louis Mullie <louis.mullie@gmail.com>
2012-05-17 14:41:34 -0400

    Add support for multiple tag sets.

--- lib/treat/lexicalizers/categorizers/from_tag.rb
+++ lib/treat/lexicalizers/categorizers/from_tag.rb
@@ -3,17 +3,19 @@
 # from its tag (e.g. 'S', 'NP', 'VBZ', 'ADV', etc.).
 class Treat::Lexicalizers::Categorizers::FromTag
 
-  Pttc = Treat::Linguistics::Tags::PhraseTagToCategory
-  Wttc = Treat::Linguistics::Tags::WordTagToCategory
-  Ptc = Treat::Linguistics::Tags::PunctuationToCategory
+  Pttc = Treat::Universalisation::Tags::PhraseTagToCategory
+  Wttc = Treat::Universalisation::Tags::WordTagToCategory
+  Ptc = Treat::Universalisation::Tags::PunctuationToCategory
   
   # Find the category of the entity from its tag.
   def self.category(entity, options = {})
 
     tag = entity.check_has(:tag)
+    
     return :unknown if tag.nil? || tag == '' || entity.type == :symbol
     return :sentence if tag == 'S' || entity.type == :sentence
     return :number if entity.type == :number
+    
     return Ptc[entity.to_s] if entity.type == :punctuation
     
     if entity.is_a?(Treat::Entities::Phrase)
@@ -29,15 +31,17 @@ class Treat::Lexicalizers::Categorizers::FromTag
     
     if entity.has?(:tag_set)
       ts = entity.get(:tag_set)
-    elsif entity.parent_phrase && 
-      entity.parent_phrase.has?(:tag_set)
-      ts = entity.parent_phrase.get(:tag_set)
     else
-      raise Treat::Exception,
-      "No information can be found regarding "+
-      "which tag set to use."
+      a = entity.ancestor_with_feature(:phrase, :tag_set)
+      if a
+        ts = a.get(:tag_set)
+      else
+        raise Treat::Exception,
+        "No information can be found regarding "+
+        "which tag set to use."
+      end
     end
-
+  
     if cat[ts]
       return cat[ts]
     else
