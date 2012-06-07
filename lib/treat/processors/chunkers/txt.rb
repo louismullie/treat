9c262dbcad6595dcc773abd3a64e640311d6e321
Louis Mullie <louis.mullie@gmail.com>
2012-06-06 20:58:36 -0400

    Improvement to allow multiple sections.

--- lib/treat/processors/chunkers/txt.rb
+++ lib/treat/processors/chunkers/txt.rb
@@ -8,12 +8,23 @@ class Treat::Processors::Chunkers::TXT
     
     entity.check_hasnt_children
     zones = entity.to_s.split("\n")
-    
+    current = entity
     zones.each do |zone|
       zone.strip!
       next if zone == ''
-      entity << Treat::Entities::
+      c = Treat::Entities::
       Zone.from_string(zone)
+      if c.type == :title
+        if current.type == :section
+          current = current.parent
+          current = entity << Treat::
+          Entities::Section.new
+        else
+          current = entity << Treat::
+          Entities::Section.new
+        end
+      end
+      current << c
     end
     
   end
