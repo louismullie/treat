302e7b9b8efb006395c4cd0f88f4abcb71bdfe1c
Louis Mullie <louis.mullie@gmail.com>
2012-06-16 01:49:28 -0400

    Consolidated linguistic configuration.

---
+++ lib/treat/config/tags/stanford.rb
@@ -0,0 +1,5 @@
+{language_to_tag_set: {
+  :french => :paris7,
+  :english => :penn,
+  :german => :stuttgart
+}}
--- lib/treat/universalisation.rb
+++
@@ -1,9 +0,0 @@
-module Treat::Universalisation
-  
-  p = 'treat/universalisation/*.rb'
-  
-  Dir[Treat.lib + p].each do |f| 
-    require f
-  end
-  
-end
