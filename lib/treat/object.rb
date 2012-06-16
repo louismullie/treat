3976be3abcf06d2b99c1e2d2cca54a930b358b1b
Louis Mullie <louis.mullie@gmail.com>
2012-06-16 01:47:58 -0400

    Consolidated all helpers together.

--- lib/treat/object.rb
+++
@@ -1,7 +0,0 @@
-# Make undefining constants publicly available on any object.
-Object.module_eval do
-  # Unset a constant without private access.
-  def self.const_unset(const)
-    Object.instance_eval { remove_const(const) }
-  end
-end
