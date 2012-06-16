ae574deaa71e750adca1ddd32d2a9edbd7bec797
Louis Mullie <louis.mullie@gmail.com>
2012-06-16 01:41:08 -0400

    Reflect inclusion of DataSet in Core module.

--- lib/treat/entities/abilities/exportable.rb
+++ lib/treat/entities/abilities/exportable.rb
@@ -1,7 +1,7 @@
 module Treat::Entities::Abilities::Exportable
   
   def export(classification)
-    ds = Treat::DataSet.new(classification)
+    ds = Treat::Core::DataSet.new(classification)
     each_entity(*classification.types) do |e|
       ds << e
     end
