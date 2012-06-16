d3bbc0bff82e455622e8b1fad47e20615c28a4ba
Louis Mullie <louis.mullie@gmail.com>
2012-06-16 01:45:32 -0400

    Moved languages to configuration files.

---
+++ lib/treat/config/languages/english.rb
@@ -0,0 +1,57 @@
+{
+  codes: {
+    two_letter: 'en',
+    three_letter: 'eng'
+  },
+  dependencies: {
+    required: ['rbtagger', 'ruby-stemmer', 'tactful-tokenizer', 'nickel', 'rwordnet'],
+    optional: ['uea-stemmer', 'engtagger', 'activesupport', 'english']
+  },
+  workers: {
+    extractors: {
+      time: [:chronic, :ruby, :nickel],
+      topics: [:reuters],
+      keywords: [:tf_idf],
+      name_tag: [:stanford],
+      coreferences: [:stanford]
+    },
+    inflectors: {
+      conjugators: [:linguistics],
+      declensors: [:english, :linguistics, :active_support],
+      stemmers: [:porter, :porter_c, :uea],
+      ordinalizers:  [:linguistics],
+      cardinalizers:  [:linguistics]
+    },
+    lexicalizers: {
+      categorizers: [:from_tag],
+      taggers: [:lingua, :brill, :stanford],
+      sensers: [:wordnet]
+    },
+    processors: {
+      parsers: [:stanford, :enju],
+      segmenters: [:tactful, :punkt, :stanford],
+      tokenizers: [:ptb, :stanford, :tactful, :punkt]
+    }
+  },
+  info: {
+    stopwords:
+      ['the', 'of', 'and', 'a', 'to', 'in', 'is',
+      'you', 'that', 'it', 'he', 'was', 'for', 'on',
+      'are', 'as', 'with', 'his', 'they', 'I', 'at',
+      'be', 'this', 'have', 'from', 'or', 'one', 'had',
+      'by', 'word', 'but', 'not', 'what', 'all', 'were',
+      'we', 'when', 'your', 'can', 'said', 'there', 'use',
+      'an', 'each', 'which', 'she', 'do', 'how', 'their',
+      'if', 'will', 'up', 'other', 'about', 'out', 'many',
+      'then', 'them', 'these', 'so', 'some', 'her', 'would',
+      'make', 'like', 'him', 'into', 'time', 'has', 'look',
+      'two', 'more', 'write', 'go', 'see', 'number', 'no',
+      'way', 'could', 'people', 'my', 'than', 'first', 'been',
+      'call', 'who', 'its', 'now', 'find', 'long', 'down',
+      'day', 'did', 'get', 'come', 'made', 'may', 'part',
+      'say', 'also', 'new', 'much', 'should', 'still',
+      'such', 'before', 'after', 'other', 'then', 'over',
+      'under', 'therefore', 'nonetheless', 'thereafter',
+      'afterwards', 'here', 'huh', 'hah', "n't", "'t", 'here']
+  }
+}
---
+++ lib/treat/config/linguistics/punctuation.rb
@@ -0,0 +1,38 @@
+{sign_to_category: {
+  
+  '.' => :period,
+  ',' => :comma,
+  ';' => :semicolon,
+  ':' => :colon,
+  '?' => :interrogation,
+  'ecl' => :interrogation, #fix!
+  '"' => :double_quote,
+  "'" => :single_quote,
+  '$' => :dollar,
+  '%' => :percent,
+  '#' => :hash,
+  '*' => :asterisk,
+  '&' => :ampersand,
+  '+' => :plus,
+  '-' => :dash,
+
+  '/' => :slash,
+  '\\' => :backslash,
+  '^' => :caret,
+  '_' => :underscore,
+  '`' => :tick,
+  '|' => :pipe,
+  '~' => :tilde,
+  '@' => :at,
+
+  '[' => :bracket,
+  ']' => :bracket,
+  '{' => :brace,
+  '}' => :brace,
+  '(' => :parenthesis,
+  ')' => :parenthesis,
+
+  '<' => :tag,
+  '>' => :tag
+  
+}}
