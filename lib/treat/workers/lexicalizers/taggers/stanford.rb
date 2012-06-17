a95b9b1f864845499586bc219bf1ebd22b0b8ff2
Louis Mullie <louis.mullie@gmail.com>
2012-06-17 01:50:13 -0400

    Reflect config change in paths.

--- lib/treat/workers/lexicalizers/taggers/stanford.rb
+++ lib/treat/workers/lexicalizers/taggers/stanford.rb
@@ -5,7 +5,7 @@ class Treat::Workers::Lexicalizers::Taggers::Stanford
 
   # Hold one tagger per language.
   @@taggers = {}
-  
+
   # Hold the default options.
   DefaultOptions =  {
     :tagger_model => nil
@@ -13,19 +13,20 @@ class Treat::Workers::Lexicalizers::Taggers::Stanford
 
   # Tag the word using one of the Stanford taggers.
   def self.tag(entity, options = {})
-    
+
     # Handle options and initialize the tagger.
     lang = entity.language
     options = get_options(options, lang)
     init_tagger(lang)
     tokens, list = get_token_list(entity)
-    
+
     # Do the tagging.
     i = 0
-   isolated_token = entity.is_a?(Treat::Entities::Token)
+    isolated_token = entity.is_a?(Treat::Entities::Token)
+    puts @@taggers[lang].inspect
     @@taggers[lang].apply(list).each do |tok|
       tokens[i].set :tag, tok.tag
-      tokens[i].set :tag_set, 
+      tokens[i].set :tag_set,
       options[:tag_set] if isolated_token
       return tok.tag if isolated_token
       i += 1
@@ -33,15 +34,13 @@ class Treat::Workers::Lexicalizers::Taggers::Stanford
 
     # Handle tags for sentences and phrases.
     if entity.is_a?(Treat::Entities::Sentence) ||
-      (entity.is_a?(Treat::Entities::Phrase) && 
+      (entity.is_a?(Treat::Entities::Phrase) &&
       !entity.parent_sentence)
-      
-        tag_set =  Treat.linguistics.tags.
-        stanford.tag_set_for_language[lang]
-        
-        entity.set :tag_set, tag_set
+
+      tag_set = options[:tag_set]
+      entity.set :tag_set, tag_set
     end
-    
+
     if entity.is_a?(Treat::Entities::Sentence)
       return 'S'
     elsif entity.is_a?(Treat::Entities::Phrase)
@@ -49,31 +48,29 @@ class Treat::Workers::Lexicalizers::Taggers::Stanford
     end
 
   end
-  
+
   # Initialize the tagger for a language.
   def self.init_tagger(language)
     Treat::Loaders::Stanford.load(language)
-    model = StanfordCoreNLP::Config::Models[:pos][language]
-    model = Treat::Loaders::Stanford.model_path +
+    model = StanfordCoreNLP::Config::Models[:pos][language] # fix
+    model = Treat.paths.models + 'stanford/' +
     StanfordCoreNLP::Config::ModelFolders[:pos] + model
-    @@taggers[language] ||= 
+    @@taggers[language] ||=
     StanfordCoreNLP::MaxentTagger.new(model)
   end
-  
+
   # Handle the options for the tagger.
   def self.get_options(options, language)
     options = DefaultOptions.merge(options)
-    options[:tag_set] = 
-    StanfordCoreNLP::Config::TagSets[language]
     if options[:tagger_model]
       ::StanfordCoreNLP.set_model('pos.model',
       options[:tagger_model])
     end
-    options[:tag_set] = 
+    options[:tag_set] =
     StanfordCoreNLP::Config::TagSets[language]
     options
   end
-  
+
   # Retrieve a Java ArrayList object.
   def self.get_token_list(entity)
     list = StanfordCoreNLP::ArrayList.new
@@ -87,5 +84,5 @@ class Treat::Workers::Lexicalizers::Taggers::Stanford
     end
     return tokens, list
   end
-  
+
 end
