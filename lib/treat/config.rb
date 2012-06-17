3e900340634e7ed03637fa0784283c5834c76a88
Louis Mullie <louis.mullie@gmail.com>
2012-06-17 01:44:42 -0400

    Refactorings and bug fixes.

--- lib/treat/config.rb
+++ lib/treat/config.rb
@@ -1,65 +1,75 @@
 module Treat::Config
 
-  # Require configatron.
-  require 'configatron'
-
   Paths = [
-    :tmp, :lib, :bin, :files, 
+    :tmp, :lib, :bin, :files,
     :data, :models, :spec
   ]
 
+  class << self
+    attr_accessor :config
+  end
+
   Treat.module_eval do
-    
     # Handle all missing methods as conf options.
     def self.method_missing(sym, *args, &block)
-      configatron.send(sym, args, block)
+      super(sym, *args, &block) if sym == :to_ary
+      Treat::Config.config[sym]
     end
-
   end
-  
+
   def self.configure
-    self.configure_paths
-    self.load_configuration
-  end
-  
-  def self.configure_paths
-    Paths.each do |path|
-      s = "configatron.paths.#{path} = '" +
-      File.dirname(__FILE__) + 
-      '/../../' + path.to_s + "/'"
-      eval s
-    end
-  end
-
-  def self.load_configuration
     # Temporary configuration hash.
-    config = {}
-    path = Treat.paths.lib + 'treat/config'
+    config = {paths: {}}
+    confdir = get_full_path(:lib) + 'treat/config'
     # Iterate over each directory in the config.
-    Dir[path + '/*'].each do |dir|
+    Dir[confdir + '/*'].each do |dir|
       name = File.basename(dir, '.*').intern
+      config[name] = {}
       # Iterate over each file in the directory.
-      Dir[path + "/#{name}/*.rb"].each do |file|
+      Dir[confdir + "/#{name}/*.rb"].each do |file|
         key = File.basename(file, '.*').intern
-        eval "configatron.#{name}.#{key}" +
-        " = #{File.read(file)}"
+        config[name][key] = eval(File.read(file))
       end
     end
-    configatron.configure_from_hash(config)
-
+    # Get the path config.
+    Paths.each do |path|
+      config[:paths][path] = get_full_path(path)
+    end
+    # Convert hash to structs.
+    self.config = self.hash_to_struct(config)
   end
-  
+
+
+  def self.get_full_path(dir)
+    File.dirname(__FILE__) +
+    '/../../' + dir.to_s + "/"
+  end
+
+  def self.hash_to_struct(hash)
+    return hash if hash.keys.
+    select { |k| !k.is_a?(Symbol) }.size > 0
+    struct = Struct.new(
+    *hash.keys).new(*hash.values)
+    hash.each do |key, value|
+      if value.is_a?(Hash)
+        struct[key] =
+        self.hash_to_struct(value)
+      end
+    end
+    struct
+  end
+
   # Turn on syntactic sugar.
   def self.sweeten!
-    return if Treat.core.syntax[:sweetened?]
-    Treat.core.syntax[:sweetened?] = true
+    return if Treat.core.syntax.sweetened
+    Treat.core.syntax.sweetened = true
     Treat.core.entities.each do |type|
       next if type == :Symbol
-      kname = type.to_s.capitalize.intern
+      kname = cc(type).intern
       klass = Treat::Entities.const_get(kname)
       Object.class_eval do
-        define_method(kname) do |value, options={}|
-          klass.build(value, options)
+        define_method(kname) do |val, opts={}|
+          klass.build(val, opts)
         end
       end
     end
@@ -67,19 +77,17 @@ module Treat::Config
 
   # Turn off syntactic sugar.
   def self.unsweeten!
-    return unless Treat.core.syntax[:sweetened?]
-    Treat.core.syntax[:sweetened?] = false
-
+    return unless Treat.core.syntax.sweetened
+    Treat.core.syntax.sweetened = false
     Treat.core.entities.each do |type|
-      kname = type.to_s.capitalize.intern
-      Object.class_eval do
-        # undef kname
-      end unless type == :symbol
+      name = cc(type).intern
+      next if type == :Symbol
+      Object.class_eval { remove_method(name) }
     end
 
   end
-  
+
   # Run all configuration.
   self.configure
-  
+
 end
