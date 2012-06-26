e06c800dac50d2b0585f645a52480f089efb192a
Louis Mullie <louis.mullie@gmail.com>
2012-06-25 22:20:08 -0400

    Code slashes.

--- lib/treat/entities/abilities/registrable.rb
+++ lib/treat/entities/abilities/registrable.rb
@@ -43,42 +43,4 @@ module Treat::Entities::Abilities::Registrable
     end
   end
 
-  def contains_id?(id)
-    
-    @registry[:id][id]
-    
-  end
-
-  def contains_value?(val)
-    
-    @registry[:value][val] ?
-    true : false
-    
-  end
-
-  def contains_type?(type1)
-    
-    return true if @registry[:type][type1]
-    
-    @registry[:type].each do |type2, count|
-      if Treat::Entities.
-        match_types[type1][type2]
-        return true
-      end
-    end
-    
-    false
-    
-  end
-
-  def contains_types?(types)
-    
-    types.each do |type|
-      return true if contains_type?(type)
-    end
-    
-    false
-    
-  end
-
 end
