124d78e7fecfede979241d65246cd801e9512b7d
Louis Mullie <louis.mullie@gmail.com>
2012-06-25 22:08:57 -0400

    Added order of entities.

--- lib/treat/config/core/entities.rb
+++ lib/treat/config/core/entities.rb
@@ -1,1 +1,2 @@
-[:entity, :unknown, :email, :url, :symbol, :sentence, :punctuation, :number, :enclitic, :word, :token, :fragment, :phrase, :paragraph, :title, :zone, :list, :block, :page, :section, :collection, :document]
+{list: [:entity, :unknown, :email, :url, :symbol, :sentence, :punctuation, :number, :enclitic, :word, :token, :fragment, :phrase, :paragraph, :title, :zone, :list, :block, :page, :section, :collection, :document],
+order: [:token, :fragment,:phrase, :sentence, :zone, :section, :document, :collection]}
