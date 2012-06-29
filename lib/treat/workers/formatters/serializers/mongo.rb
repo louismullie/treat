acb929b66fe05bfad222770a22d58ca36e1e315f
Louis Mullie <louis.mullie@gmail.com>
2012-06-29 13:00:41 -0400

    Improvements in Mongo serialization (still a bit unstable)

--- lib/treat/workers/formatters/serializers/mongo.rb
+++ lib/treat/workers/formatters/serializers/mongo.rb
@@ -11,22 +11,28 @@ class Treat::Workers::Formatters::Serializers::Mongo
   def self.serialize(entity, options = {})
     
     options = DefaultOptions.merge(options)
-    
-    unless options[:db]
+
+    if !Treat.databases.mongo.db && !options[:db]
       raise Treat::Exception,
-      'Must supply the database name.'
+      'Must supply the database name in config. ' +
+      '(Treat.databases.mongo.db = ...) or pass ' +
+      'it as a parameter to #serialize.'
     end
+    
+    @@database ||= Mongo::Connection.
+    new(Treat.databases.mongo.host).
+    db(Treat.databases.mongo.db || options[:db])
     
     type = entity.type.to_s
     types = (type == 'entity') ? 'entities' : (type + 's')
     
-    coll = Treat::Databases.connection(:mongo).collection(types)
+    coll = @@database.collection(types)
     
     entity_token = {
       :id => entity.id,
       :value => entity.value,
       :type => entity.type,
-      :children => entity.children.map { |c| c.id },
+      :children => entity.children.map { |c| [c.id, c.type] },
       :parent => (entity.has_parent? ? entity.parent.id : nil),
       :features => entity.features
     }
--- lib/treat/workers/formatters/unserializers/mongo.rb
+++ lib/treat/workers/formatters/unserializers/mongo.rb
@@ -1,7 +1,8 @@
 module Treat::Workers::Formatters::Unserializers::Mongo
 
   DefaultOptions = { :recursive => true }
-
+  require 'mongo'
+  
   def self.unserialize(entity, options={})
 
     options = DefaultOptions.merge(options)
@@ -10,17 +11,42 @@ module Treat::Workers::Formatters::Unserializers::Mongo
     types = (type == 'entity') ?
     'entities' : (type + 's')
 
-    coll = Treat::Databases.connection(:mongo).
-    collection(types)
-
-    record = coll.find(:id => entity.id.to_s)
+    if !Treat.databases.mongo.db && !options[:db]
+      raise Treat::Exception,
+      'Must supply the database name in config. ' +
+      '(Treat.databases.mongo.db = ...) or pass ' +
+      'it as a parameter to #serialize.'
+    end
     
-    entity.features = record['features']
-
+    @@database ||= Mongo::Connection.
+    new(Treat.databases.mongo.host).
+    db(Treat.databases.mongo.db || options[:db])
+    
+    coll = @@database.collection(types)
+    record = coll.find_one(:id => entity.id)
+  
+    unless record
+      raise Treat::Exception,
+      "Couldn't find record ID #{entity.id}."
+    end
+    
+    # Convert feature keys to symbols.
+    features = record['features']
+    new_feat = {}
+    features.each do |feature, value|
+      new_feat[feature.intern] = value
+    end
+    entity.features = new_feat
+    
+    # Set the entity's value.
+    entity.value = record['value']
+    
     if options[:recursive]
       record['children'].each do |c|
-        e = self.unserialize(
-        Treat::Entities::Entity.new('', id), options)
+        cid, ctype = *c
+        cklass = Treat::Entities.const_get(
+        ctype.capitalize.intern)
+        e = self.unserialize(cklass.new('', cid), options)
         entity << e
       end
     end
