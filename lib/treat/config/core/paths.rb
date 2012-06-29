0fe985d9b08db303796d0254e34220089fd30280
Louis Mullie <louis.mullie@gmail.com>
2012-06-29 16:44:37 -0400

    Update files and manifests.

--- lib/treat/config/core/paths.rb
+++ lib/treat/config/core/paths.rb
@@ -3,7 +3,6 @@
   :lib => 'class and module definitions',
   :bin => 'binary files',
   :files => 'user-saved files',
-  :data => 'data set files',
   :models => 'model files',
   :spec => 'spec test files'
 }}
