29c9fdc88e3ea0c8a39a11b9557f1f7424044b13
Louis Mullie <louis.mullie@gmail.com>
2012-06-29 12:58:58 -0400

    Moved this to config.

--- lib/treat/entities.rb
+++ lib/treat/entities.rb
@@ -1,17 +1,5 @@
 module Treat::Entities
-
-  # Require the base tree node class.
-  require 'treat/entities/node'
-  # Require the base entity class.
   require 'treat/entities/entity'
-  require 'treat/entities/list'
-  
-  # Maps Treat::Entities::X() to 
-  # Treat::Entities::X.build().
-  self.constants.each do |type|
-    define_singleton_method(type) do |value='', id=nil|
-      const_get(type).build(value, id)
-    end
-  end
-
+  p = Treat.paths.lib + 'treat/entities/*.rb'
+  Dir.glob(p).each { |f| require f }
 end
