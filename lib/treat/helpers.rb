355829357bca50f31f7a0e7c8252e44c8bbe910d
Louis Mullie <louis.mullie@gmail.com>
2012-06-29 13:00:03 -0400

    Add comment.

--- lib/treat/helpers.rb
+++ lib/treat/helpers.rb
@@ -1,3 +1,5 @@
+# The Helpers namespace contains utility
+# functions used across the library.
 module Treat::Helpers
   p = Treat.paths.lib + 'treat/helpers/*.rb'
   Dir.glob(p).each { |f| require f }
