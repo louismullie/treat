04f3d2aae6c560c082f792f74fef2c508d3f802b
Louis Mullie <louis.mullie@gmail.com>
2012-07-08 13:34:36 -0400

    Made compare_with a class method.

--- lib/treat/entities/abilities/comparable.rb
+++ lib/treat/entities/abilities/comparable.rb
@@ -6,12 +6,12 @@ module Treat::Entities::Abilities::Comparable
     
     Treat.core.entities.order.each do |type|
       klass2 = Treat::Entities.const_get(cc(type))
-      rank_a = i if self.is_a?(klass2)
+      rank_a = i if self <= klass2
       rank_b = i if klass <= klass2
       next if rank_a && rank_b
       i += 1
     end
-
+    
     return -1 if rank_a < rank_b
     return 0 if rank_a == rank_b
     return 1 if rank_a > rank_b
--- lib/treat/proxies.rb
+++ lib/treat/proxies.rb
@@ -88,7 +88,7 @@ module Treat::Core::Proxies
       dlvl = Treat.core.language.detect_at
       dklass = Treat::Entities.const_get(cc(dlvl))
       
-      if compare_with(dklass) < 1 && has_parent?
+      if self.compare_with(dklass) < 1 && has_parent?
         anc = ancestor_with_type(dlvl)
         return anc.language if anc
       end
