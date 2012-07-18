93087c71a1052ec9df8c788b6f87551a92a9cf5e
Louis Mullie <louis.mullie@gmail.com>
2012-07-18 19:19:50 -0400

    Allow initializing with (name, default) or (name, proc, default).

--- lib/treat/core/feature.rb
+++ lib/treat/core/feature.rb
@@ -6,8 +6,15 @@ class Treat::Core::Feature
   
   attr_accessor :name, :proc, :default
   
-  def initialize(name, proc = nil, default = nil)
-    @name, @proc, @default = name, proc, default
+  def initialize(name, proc_or_default = nil, default = nil)
+    @name = name
+    if proc_or_default.is_a?(Proc)
+      @proc, @default = 
+      proc_or_default, default
+    else
+      @proc = nil
+      @default = proc_or_default
+    end
   end
   
 end
