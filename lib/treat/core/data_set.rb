7762a8914964c158e6d5e0dad99abac8eea222ad
Louis Mullie <louis.mullie@gmail.com>
2012-07-18 20:50:09 -0400

    Added documentation.

--- lib/treat/core/data_set.rb
+++ lib/treat/core/data_set.rb
@@ -1,9 +1,31 @@
+# A DataSet contains an entity classification
+# problem as well as data for entities that
+# have already been classified, complete with
+# references to these entities.
 class Treat::Core::DataSet
 
+  # Used to serialize Procs.
+  silence_warnings do
+    require 'sourcify'
+  end
+  
+  # The classification problem this
+  # data set holds data for.
   attr_accessor :problem
+  # Items that have been already 
+  # classified (training data).
   attr_accessor :items
+  # References to the IDs of the
+  # original entities contained
+  # in the data set.
   attr_accessor :entities
   
+  # Initialize the DataSet. Can be 
+  # done with a Problem entity
+  # (thereby creating an empty set)
+  # or with a filename (representing
+  # a serialized data set which will
+  # then be deserialized and loaded).
   def initialize(prob_or_file)
     if prob_or_file.is_a?(String)
       ds = self.class.
@@ -17,12 +39,27 @@ class Treat::Core::DataSet
     end
   end
   
+  # Add an entity to the data set.
+  # The entity's relevant features
+  # are calculated based on the 
+  # classification problem, and a
+  # line with the results of the  
+  # calculation is added to the 
+  # data set, along with the ID
+  # of the entity.
   def <<(entity)
     @items << @problem.
     export_item(entity)
     @entities << entity.id
   end
   
+  # Marshal the data set to the supplied
+  # file name. Marshal is used for speed;
+  # other serialization options may be
+  # provided in later versions. This 
+  # method relies on the sourcify gem
+  # to transform Feature procs to strings,
+  # since procs/lambdas can't be serialized.
   def serialize(file)
     problem = @problem.dup
     problem.features.each do |feature|
@@ -40,6 +77,8 @@ class Treat::Core::DataSet
     end
   end
   
+  # Unserialize a data set file created 
+  # by using the #serialize method.
   def self.unserialize(file)
     data = Marshal.load(File.read(file))
     problem, items, entities = *data
--- lib/treat/core/feature.rb
+++ lib/treat/core/feature.rb
@@ -1,11 +1,26 @@
+# Represents a feature to be used
+# in a classification task.
 class Treat::Core::Feature
+
+  # The name of the feature. If no 
+  # proc is supplied, this assumes
+  # that the target of your classification
+  # problem responds to the method
+  # corresponding to this name.
+  attr_accessor :name
+  # A proc that can be used to perform
+  # calculations before storing a feature.
+  attr_accessor :proc
+  # The default value to be
+  attr_accessor :default
   
-  silence_warnings do
-    require 'sourcify'
-  end
-  
-  attr_accessor :name, :proc, :default
-  
+  # Initialize a feature for a classification
+  # problem. If two arguments are supplied, 
+  # the second argument is assumed to be the
+  # default value. If three arguments are 
+  # supplied, the second argument is the
+  # callback to generate the feature, and 
+  # the third one is the default value.
   def initialize(name, proc_or_default = nil, default = nil)
     @name = name
     if proc_or_default.is_a?(Proc)
