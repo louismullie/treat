31d76ec05b4114724a62bcf71dff8327afbf8a17
Louis Mullie <louis.mullie@gmail.com>
2012-07-19 15:34:03 -0400

    Added documentation.

--- lib/treat/core/problem.rb
+++ lib/treat/core/problem.rb
@@ -1,15 +1,31 @@
+# Defines a classification problem.
+# - What question are we trying to answer?
+# - What features are we going to look at
+#   to attempt to answer that question?
 class Treat::Core::Problem
 
+  # The question we are trying to answer.
   attr_accessor :question
+  # An array of features that will be 
+  # looked at in trying to answer the
+  # problem's question.
   attr_accessor :features
+  # Just the labels from the features.
   attr_accessor :labels
   
+  # Initialize the problem with a question
+  # and an arbitrary number of features.
   def initialize(question, *features)
     @question = question
     @features = features
     @labels = @features.map { |f| f.name }
   end
 
+  # Return an array of all the entity's
+  # features, as defined by the problem.
+  # If include_answer is set to true, will
+  # append the answer to the problem after
+  # all of the features.
   def export_item(e, include_answer = true)
     line = []
     @features.each do |feature|
--- lib/treat/core/question.rb
+++ lib/treat/core/question.rb
@@ -1,10 +1,23 @@
+# Defines a question to answer in the
+# context of a classification problem.
 class Treat::Core::Question
   
+  # Defines an arbitrary label for the
+  # question we are trying to answer 
+  # (e.g. is_key_sentence), which will
+  # also be used as the annotation name
+  # for the answer to the question.
   attr_accessor :name
+  # Can be :continuous or :discrete,
+  # depending on the features used.
   attr_accessor :type
+  # Defines the target of the question
+  # (e.g. :sentence, :paragraph, etc.)
   attr_accessor :target
+  # Default for the answer to the question.
   attr_accessor :default
   
+  # Initialize the question.
   def initialize(name, target, 
     type = :continuous, default = nil)
     @name, @target = name, target
