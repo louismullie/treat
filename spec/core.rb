95ee73f3743b04fe76951d5f7f44686b8abc3293
Louis Mullie <louis.mullie@gmail.com>
2012-07-21 15:51:40 -0400

    Started a spec file for the core classes.

---
+++ spec/core.rb
@@ -0,0 +1,32 @@
+require_relative '../lib/treat'
+
+describe Treat::Core::DataSet do
+=begin
+
+
+p = Problem(
+  Question(:is_key_sentence, :sentence, false),
+  Feature(:word_count, 0)
+)
+
+p2 = Problem(
+  Question(:is_key_sentence, :sentence, false),
+  Feature(:word_count, 0)
+)
+
+ds = DataSet(p)
+
+text = Paragraph("Welcome to the zoo! This is a text.")
+text2 = Paragraph("Welcome here my friend. This is well, a text.")
+
+text.do :segment, :tokenize
+text2.do :segment, :tokenize
+
+ds1 = text.export(p)
+ds2 = text2.export(p2)
+
+ds1.merge(ds2)
+
+puts ds1.inspect
+=end
+end
