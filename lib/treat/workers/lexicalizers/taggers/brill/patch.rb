c6c59d7dbe949d7b38e32390a483b0e136f48aac
Louis Mullie <louis.mullie@gmail.com>
2012-07-25 02:06:57 -0400

    No need for this ugly patch anymore.

--- lib/treat/workers/lexicalizers/taggers/brill/patch.rb
+++ lib/treat/workers/lexicalizers/taggers/brill/patch.rb
@@ -1,29 +1,3 @@
-patch = false
-
-begin
-  # This whole mess is required to deal with
-  # the fact that the 'rbtagger' gem defines
-  # a top-level module called 'Word', which
-  # will clash with the top-level class 'Word'
-  # we define when syntactic sugar is enabled.
-rescue TypeError
-  if Treat.core.syntax.sweetened
-    patch = true
-    # Unset the class Word for the duration
-    # of loading the tagger.
-    Object.const_unset(:Word); retry
-  else
-    raise Treat::Exception,
-    'Something went wrong due to a name clash with the "rbtagger" gem.' +
-    'Turn off syntactic sugar to resolve this problem.'
-  end
-ensure
-  # Reset the class Word if using syntactic sugar.
-  if Treat.core.syntax.sweetened && patch
-    Object.const_set(:Word, Treat::Entities::Word)
-  end
-end
-
 Brill::Tagger.class_eval do 
 
   def tag_tokens(tokens)
