c54f110e196809c0eedb27a81291c8279c056507
Louis Mullie <louis.mullie@gmail.com>
2012-07-29 18:31:32 -0400

    Cleaning up code; use string to load Proc.

--- lib/treat/core/feature.rb
+++ lib/treat/core/feature.rb
@@ -1,35 +1,29 @@
 # Represents a feature to be used
 # in a classification task.
 class Treat::Core::Feature
-
+  
   # The name of the feature. If no 
   # proc is supplied, this assumes
   # that the target of your classification
   # problem responds to the method
   # corresponding to this name.
   attr_reader :name
+  # The feature's default value, if nil.
+  attr_reader :default
   # A proc that can be used to perform
   # calculations before storing a feature.
   attr_accessor :proc
-  # The default value to be used.
-  attr_reader :default
+  # The proc as a string value.
+  attr_accessor :proc_string
   
-  # Initialize a feature for a classification
-  # problem. If two arguments are supplied, 
-  # the second argument is assumed to be the
-  # default value. If three arguments are 
-  # supplied, the second argument is the
-  # callback to generate the feature, and 
-  # the third one is the default value.
-  def initialize(name, proc_or_default = nil, default = nil)
-    @name = name
-    if proc_or_default.is_a?(Proc)
-      @proc, @default = 
-      proc_or_default, default
-    else
-      @proc = nil
-      @default = proc_or_default
-    end
+  require 'treat/core/hashable'
+  include Treat::Core::Hashable
+  
+  # Initialize a feature for a classification problem.
+  def initialize(name, default = nil, proc_string = nil)
+    @name, @default, @proc_string =
+    name, default, proc_string
+    @proc = proc_string ? eval(proc_string) : nil
   end
   
   # Custom comparison operator for features.
