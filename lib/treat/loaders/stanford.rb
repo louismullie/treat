05e79fe52650020f8f917a6ee3cec9c63d32ea94
Louis Mullie <louis.mullie@gmail.com>
2012-07-28 20:58:07 -0400

    Fix paths in Stanford loader.

--- lib/treat/loaders/stanford.rb
+++ lib/treat/loaders/stanford.rb
@@ -9,18 +9,20 @@ class Treat::Loaders::Stanford
   def self.load(language = nil)
     return if @@loaded
     language ||= Treat.core.language.default
-    jar_path = Treat.libraries.
-    stanford.jar_path || Treat.paths.bin
-    models_path = Treat.libraries.
-    stanford.model_path || Treat.paths.models
-    StanfordCoreNLP.jar_path =
-    "#{jar_path}stanford/"
-    StanfordCoreNLP.model_path =
-    "#{models_path}stanford/"
+    
+    StanfordCoreNLP.jar_path = 
+    Treat.libraries.stanford.jar_path || 
+    Treat.paths.bin + 'stanford/'
+    
+    StanfordCoreNLP.model_path = 
+    Treat.libraries.stanford.model_path || 
+    Treat.paths.models + 'stanford/'
+    
     StanfordCoreNLP.use(language)
     if Treat.core.verbosity.silence
       StanfordCoreNLP.log_file = NULL_DEVICE
     end
+    
     StanfordCoreNLP.bind
     @@loaded = true
   end
