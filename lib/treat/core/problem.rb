407a123f6376ca43c3d9009d8b663665450c24ef
Louis Mullie <louis.mullie@gmail.com>
2012-07-29 18:32:12 -0400

    Add parameter checks; work on serialization.

--- lib/treat/core/problem.rb
+++ lib/treat/core/problem.rb
@@ -12,13 +12,27 @@ class Treat::Core::Problem
   attr_reader :features
   # Just the labels from the features.
   attr_reader :labels
+  # A unique identifier for the problem.
+  attr_accessor :id
   
   # Initialize the problem with a question
   # and an arbitrary number of features.
   def initialize(question, *features)
+    unless question.is_a?(Treat::Core::Question)
+      raise Treat::Exception,
+      "The first argument to initialize should be " +
+      "an instance of Treat::Core::Question."
+    end
+    if features.any? { |f| !f.is_a?(Treat::Core::Feature) }
+      raise Treat::Exception,
+      "The second argument and all subsequent ones " +
+      "to initialize should be instances of Treat::" +
+      "Core::Feature."
+    end
     @question = question
     @features = features
     @labels = @features.map { |f| f.name }
+    @id = object_id
   end
   
   # Custom comparison for problems.
@@ -32,7 +46,7 @@ class Treat::Core::Problem
   # If include_answer is set to true, will
   # append the answer to the problem after
   # all of the features.
-  def export_item(e, include_answer = true)
+  def export_item(e, what = :features, include_answer = true)
     line = []
     @features.each do |feature|
       r = feature.proc ? 
@@ -44,6 +58,31 @@ class Treat::Core::Problem
     line << (e.has?(@question.name) ? 
     e.get(@question.name) : @question.default)
     line
+  end
+  
+  def to_hash
+    {'question' => @question.to_hash,
+    'features' => @features.map { |f| 
+    f.tap { |f| f.proc = nil }.to_hash },
+    'id' => @id }
+  end
+  
+  def self.from_hash(hash)
+    question = Treat::Core::Question.new(
+      hash['question']['name'], 
+      hash['question']['target'],
+      hash['question']['type'],
+      hash['question']['default']
+    )
+    features = []
+    hash['features'].each do |feature|
+      features << Treat::Core::Feature.new(
+      feature['name'], feature['default'],
+      feature['proc_string'])
+    end
+    p = Treat::Core::Problem.new(question, *features)
+    p.id = hash['id']
+    p
   end
 
 end
