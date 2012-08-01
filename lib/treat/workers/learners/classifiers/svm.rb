4216a8ac28ac28be2cdc280ae7b399c9223d6742
Louis Mullie <louis.mullie@gmail.com>
2012-08-01 03:04:40 -0400

    Edited variable names for clarity.

--- lib/treat/workers/learners/classifiers/svm.rb
+++ lib/treat/workers/learners/classifiers/svm.rb
@@ -7,24 +7,24 @@ class Treat::Workers::Learners::Classifiers::SVM
   def self.classify(entity, options = {})
     
     set = options[:training]
-    cl = set.problem
+    problem = set.problem
     
-    if !@@classifiers[cl]
-      labels = set.question.labels
+    if !@@classifiers[problem]
+      labels = problem.question.labels
       unless labels
         raise Treat::Exception,
         "LibSVM requires that you provide the possible " +
         "labels to assign to classification items when " +
         "specifying the question."
       end
-      data = set.items.map { |item| item[:features] }
+      data = set.items.map  { |item| item[:features] }
       prob = Problem.new(labels, data)
-      param = Parameter.new(:kernel_type => LINEAR, :C => 10)
-      @@classifiers[cl] = Model.new(prob, param)
+      param = Parameter.new(:kernel_type => LINEAR, :C => 10)
+      @@classifiers[problem] = Model.new(prob, param)
     end
-    
-    @@classifiers[cl].predict_probability(
-    cl.export_item(entity, false))
+
+    @@classifiers[problem].predict_probability(
+    problem.export_features(entity, false))[0]
     
   end
   
