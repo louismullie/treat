18000b65b0766d86580b38a5486d86efe436e790
Louis Mullie <louis.mullie@gmail.com>
2012-08-02 20:33:44 -0400

    Add a helper function for the specs.

---
+++ spec/helper.rb
@@ -0,0 +1,33 @@
+require 'simplecov'
+
+SimpleCov.start do
+
+  add_filter '/spec/'
+  add_filter '/config/'
+
+  add_group 'Core', 'treat/core'
+  add_group 'Entities', 'treat/entities'
+  add_group 'Helpers', 'treat/helpers'
+  add_group 'Loaders', 'treat/loaders'
+  add_group 'Workers', 'treat/workers'
+  add_group 'Config', 'config.rb'
+  add_group 'Proxies', 'proxies.rb'
+  add_group 'Treat', 'treat.rb'
+
+end
+
+require_relative '../lib/treat'
+
+Treat.core.language.detect = true
+Treat.core.verbosity.debug = true
+Treat.paths.files = './files/'
+Treat.databases.mongo.db = 'gistify'
+Treat.databases.default.adapter = :mongo
+Treat.libraries.stanford.model_path = 
+'/ruby/stanford/stanford-core-nlp-all/'
+Treat.libraries.stanford.jar_path =
+'/ruby/stanford/stanford-core-nlp-all/'
+Treat.libraries.punkt.model_path = 
+'/ruby/punkt/'
+Treat.libraries.reuters.model_path =
+'/ruby/reuters/'
