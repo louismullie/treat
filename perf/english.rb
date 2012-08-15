28a767e9ffbdc11cafb9adb39b0c77be5c214ff5
Louis Mullie <louis.mullie@gmail.com>
2012-08-15 18:25:12 -0400

    More work on automated tests.

--- perf/english.rb
+++ perf/english.rb
@@ -6,6 +6,8 @@ Treat.libraries.stanford.jar_path =
 '/ruby/stanford/stanford-core-nlp-all/'
 Treat.libraries.punkt.model_path =
 '/ruby/punkt/'
+Treat.libraries.reuters.model_path =
+'/ruby/reuters/'
 
 module Treat::Benchmarks
   require 'benchmark'
@@ -16,6 +18,9 @@ end
 
 # class Treat::Benchmarks::Agnostic; end
 class Treat::Benchmarks::English
+  Headings = ['Task', 'Worker',
+    'Description', 'Reference', 'User',
+  'System', 'Real', 'Accuracy']
   Benchmarks = {
     tokenize: {
       group: {
@@ -118,7 +123,74 @@ class Treat::Benchmarks::English
         ],
         generator: lambda { |entity| entity.time.month }
       }
-    }
+    },
+    topics: {
+      document: {
+        examples: [
+          ["./perf/examples/test.txt", ['household goods and hardware', 'united states of america', 'corporate/industrial']]
+        ],
+        preprocessor: lambda { |doc| doc.do :chunk, :segment, :tokenize }
+      },
+      section: {
+        # Must implement
+      },
+      zone: {
+        examples: [
+          ["Michigan, Ohio, Texas - Unfortunately, the RadioShack is closing. This is horrible news for U.S. politics.", ['household goods and hardware', 'united states of america', 'corporate/industrial']]
+        ],
+        preprocessor: lambda { |zone| zone.do :segment, :tokenize }
+      }
+    },
+    topic_words: {
+      collection: {
+        examples: [
+          ["./perf/examples/economist", [""]]
+        ],
+        preprocessor: lambda { |coll| coll.do :chunk, :segment, :tokenize }
+      }
+    },
+    conjugate: {
+      word: {
+        examples: {
+          present_participle: [
+            ["run", "running"]
+          ],
+          infinitive: [
+            ["running", "run"]
+          ]
+        }
+      }
+    },
+    declense: {
+      word: {
+        examples: {
+          singular: [
+            ["men", "man"]
+          ],
+          plural: [
+            ["runs", "run"]
+          ]
+        }
+      }
+    },
+    sense: {
+      word: {
+        examples: {
+          synonyms: [
+            ["throw", ["throw", "shed", "cast", "cast_off", "shake_off", "throw_off", "throw_away", "drop", "thrust", "give", "flip", "switch", "project", "contrive", "bewilder", "bemuse", "discombobulate", "hurl", "hold", "have", "make", "confuse", "fox", "befuddle", "fuddle", "bedevil", "confound"],]
+          ],
+          antonyms: [
+            ["weak", ["strong"]]
+          ],
+          hypernyms: [
+            ["table", ["array", "furniture", "piece_of_furniture", "article_of_furniture", "tableland", "plateau", "gathering", "assemblage", "fare"]]
+          ],
+          hyponyms: [
+            ["furniture", ["baby_bed", "baby's_bed", "bedroom_furniture", "bedstead", "bedframe", "bookcase", "buffet", "counter", "sideboard", "cabinet", "chest_of_drawers", "chest", "bureau", "dresser", "dining-room_furniture", "etagere", "fitment", "hallstand", "lamp", "lawn_furniture", "nest", "office_furniture", "seat", "sectional", "Sheraton", "sleeper", "table", "wall_unit", "wardrobe", "closet", "press", "washstand", "wash-hand_stand"]]
+          ]
+        }
+      }
+    },
   }
   def self.run
 
@@ -134,7 +206,16 @@ class Treat::Benchmarks::English
         group_class = Treat::Workers.
         const_get(cc(cat)).
         const_get(cc(grp))
-        next unless [:segment, :language, :category].include?(group_class.method)   # :segment, :tokenize, :tag, :ordinal, :cardinal, :name_tag, :stem, :time,
+        puts group_class.method
+        next unless [:category].
+        include?(group_class.method)
+        # ??: :language
+        # TODO: :keywords, :tf_idf, :topic_words
+        # :read, :visualize, :serialize, :unserialize, :search, 
+        # :index, :sense, :parse, :classify
+        # DONE: :segment, :tokenize, :tag, :ordinal, :cardinal, 
+        # :name_tag, :stem, :time, :topics, :conjugate, :declense,
+        # :sense, :category
         group.each do |worker|
           results << self.run_benchmark(
           group_class, worker)
@@ -143,6 +224,7 @@ class Treat::Benchmarks::English
     end
 
     print_table(results)
+    save_html(results)
 
   end
 
@@ -159,23 +241,25 @@ class Treat::Benchmarks::English
       i = 0; n = 0
 
       targets.each do |target|
+        next if target == :section ### FIXME
         benchmark = Benchmarks[method][target]
         examples = benchmark[:examples]
         i2 = 0; n2 = 0
         if examples.is_a?(Hash)
-          examples.each do |preset, example|
-            options = {group_class.preset => preset}
+          preset_examples = benchmark[:examples]
+          preset_examples.each do |preset, examples|
+            options = {group_class.preset_option => preset}
+            bm = benchmark.dup; bm[:examples] = examples
             i2, n2 = *self.run_examples(method,
-            worker, target, benchmark, options)
+            worker, target, bm, options)
           end
         else
-          puts "yes"
           i2, n2 = *self.run_examples(method,
           worker, target, benchmark)
         end
         i += i2; n += n2
       end
-      puts [i, n].inspect
+
       accuracy = (i.to_f/n.to_f*100).round(2)
 
     end
@@ -186,7 +270,7 @@ class Treat::Benchmarks::English
       time.utime.round(4).to_s,
       time.stime.round(4).to_s,
       time.real.round(4).to_s,
-      accuracy ]
+    accuracy ]
 
   end
 
@@ -200,37 +284,50 @@ class Treat::Benchmarks::English
     benchmark[:preprocessor]
     target_class = Treat::Entities.
     const_get(cc(target))
-    
+
     examples.each do |example|
       value, expectation = *example
-      entity = target_class.new(value)
-      if preprocessor
-        entity = preprocessor.call(entity)
-      end
-      
+      entity = target_class.build(value)
+      preprocessor.call(entity) if preprocessor
       if generator
         entity.send(method, worker, options)
         result = generator.call(entity)
         i += 1 if result == expectation
       else
-        i += 1 if example.send(method,
+        i += 1 if entity.send(method,
         worker, options) == expectation
       end
-      
+
       n += 1
     end
 
     [i, n]
 
   end
-  
-  
+
+
   def self.print_table(rows)
-    headings = ['Task', 'Worker',
-      'Description', 'Reference', 'User',
-    'System', 'Real', 'Accuracy']
     puts Terminal::Table.new(
-    headings: headings, rows: rows)
+    headings: Headings, rows: rows)
+  end
+
+  def self.save_html(rows)
+    html = "<table>\n"
+    html += "<tr>\n"
+    Headings.each do |heading|
+      html += "<td>" + heading + "</td>\n"
+    end
+    html += "</tr>\n"
+    rows.each do |row|
+      html += "<tr>\n"
+      row.each do |el|
+        html += "<td>#{el}</td>"
+      end
+      html += "</tr>\n"
+    end
+    File.open('report.html', 'w') do |f|
+      f.write(html)
+    end
   end
 
   def self.get_description(group_class, worker)
@@ -243,6 +340,7 @@ class Treat::Benchmarks::English
     gsub('encoding: utf-8', '').
     gsub(/Authors: (.*)/m, '').
     gsub(/License: (.*)/m, '').
+    gsub(/Website: (.*)/m, '').
     split('Original paper: ')
   end
 
