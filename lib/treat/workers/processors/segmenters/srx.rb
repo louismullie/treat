fc092b8670216a6845838e73fc283a6499330808
Louis Mullie <louis.mullie@gmail.com>
2012-08-16 12:16:55 -0400

    Add support for SRX gems for other languages.

--- lib/treat/workers/processors/segmenters/srx.rb
+++ lib/treat/workers/processors/segmenters/srx.rb
@@ -1,34 +1,42 @@
-# Sentence segmentation based on a set of predefined 
+# Sentence segmentation based on a set of predefined
 # rules defined in SRX (Segmentation Rules eXchange)
 # format and developped by Marcin Milkowski.
 #
-# Original paper: Marcin Miłkowski, Jarosław Lipski, 
-# 2009. Using SRX standard for sentence segmentation 
-# in LanguageTool, in: Human Language Technologies 
+# Original paper: Marcin Miłkowski, Jarosław Lipski,
+# 2009. Using SRX standard for sentence segmentation
+# in LanguageTool, in: Human Language Technologies
 # as a Challenge for Computer Science and Linguistics.
 class Treat::Workers::Processors::Segmenters::SRX
-  
-  # Require the srx-english library.
-  require 'srx/english/sentence_splitter'
 
+  @@segmenters = {}
+
+  # Require the srx-english library.
   # Segment a text using the SRX algorithm
   def self.segment(entity, options = {})
-    
+
+    lang = entity.language
     entity.check_hasnt_children
     text = entity.to_s
     escape_floats!(text)
-    
-    sentences = SRX::English::
-    SentenceSplitter.new(text)
-    
+
+    unless @@segmenters[lang]
+      # Require the appropriate gem.
+      require "srx/#{lang}/sentence_splitter"
+      @@segmenters[lang] = SRX.const_get(
+      lang.capitalize).const_get(
+      'SentenceSplitter')
+    end
+
+    sentences = @@segmenters[lang].new(text)
+
     sentences.each do |sentence|
       unescape_floats!(sentence)
       entity << Treat::Entities::Phrase.
       from_string(sentence.strip)
     end
-    
+
     entity
-    
+
   end
 
 end
