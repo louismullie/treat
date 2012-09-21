e8a163a9aeeba42e554febf1e848d949b8917d0e
Louis Mullie <louis.mullie@gmail.com>
2012-09-21 01:05:06 -0400

    Remodel declension behaviour based on category.

--- lib/treat/workers/inflectors/declensors/linguistics.rb
+++ lib/treat/workers/inflectors/declensors/linguistics.rb
@@ -7,43 +7,48 @@ class Treat::Workers::Inflectors::Declensors::Linguistics
 
   require 'treat/loaders/linguistics'
 
+
   # Retrieve a declension of a word using the 'linguistics' gem.
   #
   # Options:
   #
   # - (Identifier) :count => :singular, :plural
   def self.declense(entity, options = {})
-    
-    cat = entity.check_has(:category)
-    unless ['noun', 'adjective', 'determiner'].
-      include?(cat)
-        return
-    end
-    
+
+    cat = entity.get(:category)
+    return if cat && !['noun', 'adjective',
+    'determiner'].include?(cat)
     unless options[:count]
       raise Treat::Exception,
-      "Must supply option count (:singular or :plural)."
+      "Must supply :count option (:singular or :plural)."
     end
-    
-    klass = Treat::Loaders::Linguistics.load(entity.language)
+
+    klass = Treat::Loaders::
+    Linguistics.load(entity.language)
+
     string = entity.to_s
-    
+
     if options[:count] == 'plural'
-      
-      if entity.has?(:category) &&
-        ['noun', 'adjective', 'verb'].
-        include?(entity.category)
+
+      if (entity.has?(:category))
+        result = ''
         silence_warnings do
-          klass.send(
-          :"plural_#{entity.category}", 
+          result = klass.send(
+          :"plural_#{entity.category}",
           string)
         end
+        return result
+
       else
-        klass.plural(string)
+        return klass.plural(string)
       end
-      
+
+    else
+      raise Treat::UnsupportedException,
+      "Ruby Linguistics does not support " +
+      "singularization of words."
     end
-    
+
   end
 
 end
