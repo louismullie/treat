61e2a919c5b3b6212a6031afde288c8d1925dc6d
Louis Mullie <louis.mullie@gmail.com>
2012-09-23 03:27:14 -0400

    Add a few stop words.

--- lib/treat/config/languages/english.rb
+++ lib/treat/config/languages/english.rb
@@ -36,25 +36,24 @@
       tokenizers: [:ptb, :stanford, :punkt]
     }
   },
-  info: {
-    stopwords:
-      ['the', 'of', 'and', 'a', 'to', 'in', 'is',
-      'you', 'that', 'it', 'he', 'was', 'for', 'on',
-      'are', 'as', 'with', 'his', 'they', 'I', 'at',
-      'be', 'this', 'have', 'from', 'or', 'one', 'had',
-      'by', 'word', 'but', 'not', 'what', 'all', 'were',
-      'we', 'when', 'your', 'can', 'said', 'there', 'use',
-      'an', 'each', 'which', 'she', 'do', 'how', 'their',
-      'if', 'will', 'up', 'other', 'about', 'out', 'many',
-      'then', 'them', 'these', 'so', 'some', 'her', 'would',
-      'make', 'like', 'him', 'into', 'time', 'has', 'look',
-      'two', 'more', 'write', 'go', 'see', 'number', 'no',
-      'way', 'could', 'people', 'my', 'than', 'first', 'been',
-      'call', 'who', 'its', 'now', 'find', 'long', 'down',
-      'day', 'did', 'get', 'come', 'made', 'may', 'part',
-      'say', 'also', 'new', 'much', 'should', 'still',
-      'such', 'before', 'after', 'other', 'then', 'over',
-      'under', 'therefore', 'nonetheless', 'thereafter',
-      'afterwards', 'here', 'huh', 'hah', "n't", "'t", 'here']
-  }
+  stop_words:
+    ['the', 'of', 'and', 'a', 'to', 'in', 'is',
+    'you', 'that', 'it', 'he', 'was', 'for', 'on',
+    'are', 'as', 'with', 'his', 'they', 'I', 'at',
+    'be', 'this', 'have', 'from', 'or', 'one', 'had',
+    'by', 'word', 'but', 'not', 'what', 'all', 'were',
+    'we', 'when', 'your', 'can', 'said', 'there', 'use',
+    'an', 'each', 'which', 'she', 'do', 'how', 'their',
+    'if', 'will', 'up', 'other', 'about', 'out', 'many',
+    'then', 'them', 'these', 'so', 'some', 'her', 'would',
+    'make', 'like', 'him', 'into', 'time', 'has', 'look',
+    'two', 'more', 'write', 'go', 'see', 'number', 'no',
+    'way', 'could', 'people', 'my', 'than', 'first', 'been',
+    'call', 'who', 'its', 'now', 'find', 'long', 'down',
+    'day', 'did', 'get', 'come', 'made', 'may', 'part',
+    'say', 'also', 'new', 'much', 'should', 'still',
+    'such', 'before', 'after', 'other', 'then', 'over',
+    'under', 'therefore', 'nonetheless', 'thereafter',
+    'afterwards', 'here', 'huh', 'hah', "n't", "'t", 'here',
+    'neither', 'towards']
 }
