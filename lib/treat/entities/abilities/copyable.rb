5755db44ee44296178cfd891491aa625842660fb
Louis Mullie <louis.mullie@gmail.com>
2012-09-23 03:27:49 -0400

    Do not try to copy the file if it's already in the collection.

--- lib/treat/entities/abilities/copyable.rb
+++ lib/treat/entities/abilities/copyable.rb
@@ -29,6 +29,7 @@ module Treat::Entities::Abilities::Copyable
     f = File.dirname(folder)
     f = f.split(File::SEPARATOR)[-1]
     f = File.join(collection.folder, f)
+    return if File.readable?(f)
     FileUtils.mkdir(f) unless
     FileTest.directory(f)
     FileUtils.cp_r(folder, f)
@@ -41,6 +42,7 @@ module Treat::Entities::Abilities::Copyable
     return copy unless file
     f = File.basename(file)
     f = File.join(collection.folder, f)
+    return if File.readable?(f)
     FileUtils.cp(file, f)
     copy.set :file, f
     copy
