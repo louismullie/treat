bf222d8487207760034560c01d1f82484f3943f8
Louis Mullie <louis.mullie@gmail.com>
2012-09-29 20:10:40 -0400

    Stylistic consistency.

--- spec/treat.rb
+++ spec/treat.rb
@@ -2,44 +2,36 @@ require_relative 'helper'
 
 describe Treat do
 
-  describe "Syntactic sugar:" # Move to config?
+  describe "Syntactic sugar:" do
+    
+    describe "#sweeten!, #unsweeten!" do
+      it "respectively turn on and off syntactic sugar and " +
+      "define/undefine entity builders as uppercase methods " +
+      "in the global namespace" do
+        Treat.core.entities.list.each do |type|
+          next if type == :symbol
 
-  describe "#sweeten!, #unsweeten!" do
-
-    it "respectively turn on and off syntactic sugar and " +
-    "define/undefine entity builders as uppercase methods " +
-    "in the global namespace" do
-
-      Treat.core.entities.list.each do |type|
-
-        next if type == :symbol
-
-        Treat::Config.sweeten!
-
-        Treat.core.syntax.sweetened.should eql true
-
-        Object.method_defined?(
-        :"#{type.to_s.capitalize}").
-        should eql true
-
-        Treat::Config.unsweeten!
-        Treat.core.syntax.sweetened.should eql false
-
-        Object.method_defined?(
-        type.to_s.capitalize.intern).should eql false
-
-        Object.method_defined?(
-        :"#{type.to_s.capitalize}").
-        should eql false
+          Treat::Config.sweeten!
+          Treat.core.syntax.sweetened.should eql true
+          Object.method_defined?(
+          :"#{type.to_s.capitalize}").
+          should eql true
         
+          Treat::Config.unsweeten!
+          Treat.core.syntax.sweetened.should eql false
+          Object.method_defined?(
+          type.to_s.capitalize.intern).should eql false
+          Object.method_defined?(
+          :"#{type.to_s.capitalize}").
+          should eql false        
+        end
       end
-
     end
-
+    
   end
-
+  
   describe "Paths:" do
-
+    
     paths = Treat.core.paths.description
     # Check IO for bin, files, tmp, models. Fix.
     paths.each_pair do |path, files|
@@ -49,7 +41,7 @@ describe Treat do
         end
       end
     end
-
+    
   end
 
 end
