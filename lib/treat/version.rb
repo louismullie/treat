b44d407ccb2d90e29322f6a65288314e002883ef
Louis Mullie <louis.mullie@gmail.com>
2012-09-29 23:10:35 -0400

    Stylistic consistency.

--- lib/treat/version.rb
+++ lib/treat/version.rb
@@ -1,9 +1,12 @@
 module Treat
+  
   # The current version of Treat.
   VERSION = "1.2.0"
+  
   # Treat requires Ruby >= 1.9.2
   if RUBY_VERSION < '1.9.2'
     raise "Treat requires Ruby version 1.9.2 " +
     "or higher, but current is #{RUBY_VERSION}."
   end
+  
 end
