c588218b2e302cdc1da141e76faaf070927fa7bb
Louis Mullie <louis.mullie@gmail.com>
2012-09-30 00:31:26 -0400

    Big refactorings, liking the signal to noise ratio in this class now.

--- lib/treat/config/config.rb
+++ lib/treat/config/config.rb
@@ -1,12 +1,13 @@
+# This module uses structs to represent the 
+# configuration options that are stored in 
+# the /config folder.
 module Treat::Config
+  
+  # Store all the configuration in self.config
+  class << self; attr_accessor :config; end
 
-  Paths = [ :tmp, :lib, :bin, 
-  :files, :data, :models, :spec ]
-
-  class << self
-    attr_accessor :config
-  end
-
+  # Setup a proxy on the main Treat module to 
+  # make configuration options directly accessible.
   Treat.module_eval do
     # Handle all missing methods as conf options.
     # Instead, should dynamically define them. FIXME.
@@ -16,32 +17,98 @@ module Treat::Config
     end
   end
 
-  def self.configure
+  # Main function; loads all configuration options.
+  def self.configure!
     # Temporary configuration hash.
     config = { paths: {} }
-    confdir = Dir.pwd + '/lib/treat/config'
+    conf_dir = Dir.pwd + '/lib/treat/config'
     # Iterate over each directory in the config.
-    Dir[confdir + '/*'].each do |dir|
+    Dir[conf_dir + '/*'].each do |dir|
       name = File.basename(dir, '.*').intern
-      next if name == :config
-      config[name] = {}
+      next if name == :config; config[name] = {}
       # Iterate over each file in the directory.
-      Dir[confdir + "/#{name}/*.rb"].each do |file|
+      Dir[conf_dir + "/#{name}/*.rb"].each do |file|
         key = File.basename(file, '.*').intern
         config[name][key] = eval(File.read(file))
       end
     end
-    # Get the path config.
-    Paths.each do |path|
-      config[:paths][path] = Dir.pwd + '/' + path.to_s + '/'
-    end
+    # Configure all options in Treat.paths.
+    config[:paths] = self.get_paths_config
     # Get the tag alignments.
-    configure_tags!(config[:tags][:aligned])
-    # Convert hash to structs.
+    self.gen_tag_maps!(config[:tags][:aligned])
+    # Convert the hash to nested structs for perf.
     self.config = self.hash_to_struct(config)
   end
+
+  # Turn on syntactic sugar. This means that
+  # all entities and learning classes can be 
+  # created in a "Python-like" syntax in the 
+  # global namespace (e.g. Word('hello'), 
+  # Phrase('a bird'), etc.).
+  def self.sweeten!
+    return if Treat.core.syntax.sweetened
+    Treat.core.syntax.sweetened = true
+    self.sweeten_entities
+    self.sweeten_learning
+  end
+
+  # Turn off syntactic sugar. See #sweeten!
+  def self.unsweeten!
+    self.sweeten_entities(false)
+    self.sweeten_learning(false)
+  end
   
-  def self.configure_tags!(config)
+  # * Helper methods for path config * #
+  
+  # Get the path configuration based on the 
+  # directory structure loaded into Paths.
+  def self.get_paths_config
+    Hash[
+    # Get a list of directories in treat/
+    Dir.glob(Dir.pwd + '/*').select do |path|
+      FileTest.directory?(path)
+    # Map to pairs of [:name, path]
+    end.map do |path|
+      [File.basename(path).intern, path + '/']
+    end]
+  end
+  
+  # * Helper methods for syntactic sugar * #
+  
+  # Map all classes in Treat::Entities to
+  # a global builder function (Entity, etc.)
+  def self.sweeten_entities(off = false)
+    Treat.core.entities.list.each do |type|
+      next if type == :Symbol
+      kname = cc(type).intern
+      klass = Treat::Entities.const_get(kname)
+      Object.class_eval do
+        define_method(kname) do |val, opts={}|
+          klass.build(val, opts)
+        end if !off
+        remove_method(name) if off 
+      end
+    end
+  end
+  
+  # Map all classes in the Learning module
+  # to a global builder function (e.g. DataSet).
+  def self.sweeten_learning(off = false)
+    Treat::Learning.constants.each do |kname|
+      Object.class_eval do
+        define_method(kname) do |*args| 
+          Treat::Learning.const_get(kname).new(*args)
+        end if !off
+        remove_method(name) if off
+      end
+    end
+  end
+  
+  # * Helper methods for tag set config * #
+  
+  # Generate a map of word and phrase tags 
+  # to their syntactic category, by tag set. 
+  def self.gen_tag_maps!(config)
     ts = config[:tag_sets]
     config[:word_tags_to_category] = 
     align_tags(config[:word_tags], ts)
@@ -49,7 +116,7 @@ module Treat::Config
     align_tags(config[:phrase_tags], ts)
   end
 
-  # Align tag configuration.
+  # Align tag tags in the tag set 
   def self.align_tags(tags, tag_sets)
     wttc = {}
     tags.each_slice(2) do |desc, tags|
@@ -60,73 +127,24 @@ module Treat::Config
         wttc[tags[i]] ||= {}
         wttc[tags[i]][tag_set] = category
       end
-    end
-    wttc
+    end; return wttc
   end
   
+  # * General helper methods * #
+  
+  # Converts a hash to nested structs.
   def self.hash_to_struct(hash)
-    return hash if hash.keys.
-    select { |k| !k.is_a?(Symbol) }.size > 0
-    struct = Struct.new(
-    *hash.keys).new(*hash.values)
-    hash.each do |key, value|
-      if value.is_a?(Hash)
-        struct[key] =
-        self.hash_to_struct(value)
-      end
-    end
-    struct
-  end
-
-  # Turn on syntactic sugar.
-  def self.sweeten!
-    
-    # Undo this in unsweeten! - # Fix
-    Treat::Entities.module_eval do 
-      self.constants.each do |type|
-        define_singleton_method(type) do |value='', id=nil|
-          const_get(type).build(value, id)
-        end
-      end
-    end
-    
-    return if Treat.core.syntax.sweetened
-    Treat.core.syntax.sweetened = true
-    Treat.core.entities.list.each do |type|
-      next if type == :Symbol
-      kname = cc(type).intern
-      klass = Treat::Entities.const_get(kname)
-      Object.class_eval do
-        define_method(kname) do |val, opts={}|
-          klass.build(val, opts)
-        end
-      end
-    end
-    
-    Treat::Learning.constants.each do |kname|
-      Object.class_eval do
-        klass = Treat::Learning.const_get(kname)
-        define_method(kname) do |*args|
-          klass.new(*args)
-        end
-      end
-    end
-    
-  end
-
-  # Turn off syntactic sugar.
-  def self.unsweeten!
-    return unless Treat.core.syntax.sweetened
-    Treat.core.syntax.sweetened = false
-    Treat.core.entities.list.each do |type|
-      name = cc(type).intern
-      next if type == :Symbol
-      Object.class_eval { remove_method(name) }
-    end
-
-  end
-
-  # Run all configuration.
-  self.configure
+     return hash if hash.keys.
+     select { |k| !k.is_a?(Symbol) }.size > 0
+     struct = Struct.new(*hash.keys).new(*hash.values)
+     hash.each do |key, value|
+       if value.is_a?(Hash)
+         struct[key] = self.hash_to_struct(value)
+       end
+     end; return struct
+   end
+  
+  # * Load all configuration ! * #
+  self.configure!
   
 end
