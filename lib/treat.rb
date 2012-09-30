dba5f527dcc481e45aa8c2116e6321b316406292
Louis Mullie <louis.mullie@gmail.com>
2012-09-29 20:49:00 -0400

    Simplify the main script file as much as possible.

--- lib/treat.rb
+++ lib/treat.rb
@@ -1,37 +1,9 @@
 module Treat
   
-  # Require the current version.
-  require 'treat/version'
-  
-  # Custom exception class.
-  class Exception < ::Exception; end
-  class UnimplementedException < Exception; end
-  
-  # Base module with auto-require.
-  module Module
-    def self.included(base)
-      bits = base.ancestors[0].to_s.split('::').
-      collect! { |bit| bit.downcase }
-      Dir.glob(Treat.paths.lib +
-      bits.join('/') + '/*.rb').
-      each { |f| require_relative f }
-    end
-  end
-
-  # Load configuration options.
-  require_relative 'treat/config'
-  # Load all workers.
-  require_relative 'treat/helpers'
-  # Require library loaders.
-  require_relative 'treat/loaders'
-  # Require all core classes.
-  require_relative 'treat/core'
-  # Require all entity classes.
-  require_relative 'treat/entities'
-  # Lazy load worker classes.
-  require_relative 'treat/workers'
-  # Require proxies last.
-  require_relative 'treat/proxies'
+  # Load all necessary files.
+  require_relative 'treat/version'
+  require_relative 'treat/exception'
+  require_relative 'treat/modules'
   
   # Enable entity creation DSL.
   Treat::Config.sweeten!
