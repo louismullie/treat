07c4cdb6251f2cefa4fdbed2b573e9c9fd8b71dc
Louis Mullie <louis.mullie@gmail.com>
2012-09-30 01:17:36 -0400

    Add documentation.

--- lib/treat/entities/entity/comparable.rb
+++ lib/treat/entities/entity/comparable.rb
@@ -1,9 +1,11 @@
+# Allow comparison of entity hierarchy in DOM.
 module Treat::Entities::Entity::Comparable
 
+  # Determines whether the receiving class
+  # is smaller, equal or greater in the DOM
+  # hierarchy compared to the supplied one.
   def compare_with(klass)
-    
     i = 0; rank_a = nil; rank_b = nil
-    
     Treat.core.entities.order.each do |type|
       klass2 = Treat::Entities.const_get(cc(type))
       rank_a = i if self <= klass2
@@ -11,11 +13,9 @@ module Treat::Entities::Entity::Comparable
       next if rank_a && rank_b
       i += 1
     end
-    
     return -1 if rank_a < rank_b
     return 0 if rank_a == rank_b
     return 1 if rank_a > rank_b
-    
   end
 
 end
