8880ff49b7cbd61fd677657c34fc1d92ab0386dc
Louis Mullie <louis.mullie@gmail.com>
2012-09-29 21:36:36 -0400

    Put module abilities under Modules::Module.

--- lib/treat/modules.rb
+++ lib/treat/modules.rb
@@ -1,42 +1,43 @@
 module Treat
   
-  # Load the basic Module functionality.
-  require_relative 'module'
+  module Modules
+    require_relative 'modules/module'
+  end
   
   # Contains configuration.
   module Config
-    require_relative 'config'
+    require_relative 'modules/config'
   end
 
   # Contains utility functions used by Treat.
   module Helpers
-    include Module::Autoloaded
+    include Modules::Module::Autoloaded
   end
 
   # Contains classes to load external libraries.
   module Loaders
-    include Module::Autoloadable
+    include Modules::Module::Autoloadable
   end
 
   # Contains the core classes used by Treat.
   module Learning
-    include Module::Autoloaded
+    include Modules::Module::Autoloaded
   end
 
   # Contains the textual model used by Treat.
   module Entities
     require_relative 'entities/entity'
-    include Module::Autoloaded
+    include Modules::Module::Autoloaded
   end
 
   # This module creates all the worker categories.
   module Workers
-    require_relative 'workers'
+    require_relative 'modules/workers'
   end
 
   # Proxies install builders on core Ruby objects.
   module Proxies
-    require_relative 'proxies'
+    require_relative 'modules/proxies'
   end
   
 end
