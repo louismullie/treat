05d5b850105f89e98241a7876493cb848376da6f
Louis Mullie <louis.mullie@gmail.com>
2012-09-29 23:49:51 -0400

    Add description of toolkit, author & license information.

--- lib/treat.rb
+++ lib/treat.rb
@@ -1,11 +1,24 @@
+# Treat is a toolkit for natural language 
+# processing and computational linguistics 
+# in Ruby. The Treat project aims to build 
+# a language- and algorithm- agnostic NLP 
+# framework for Ruby with support for tasks 
+# such as document retrieval, text chunking, 
+# segmentation and tokenization, natural 
+# language parsing, part-of-speech tagging, 
+# keyword mining and named entity recognition.
+#
+# Author: Louis-Antoine Mullie (c) 2010-12.
+# 
+# Released under the General Public License.
 module Treat
   
-  # Load all necessary files.
+  # * Load all the core classes. * #
   require_relative 'treat/version'
   require_relative 'treat/exception'
   require_relative 'treat/modules'
   
-  # Enable DSL.
+  # * Enable syntactic sugar. * #
   Treat::Config.sweeten!
   
 end
