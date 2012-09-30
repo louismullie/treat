f879a7e23a051fff633fcdf675b094c9c428f56b
Louis Mullie <louis.mullie@gmail.com>
2012-09-29 22:51:59 -0400

    Reflect name change in autoload.

--- lib/treat/entities/entity.rb
+++ lib/treat/entities/entity.rb
@@ -11,7 +11,7 @@ module Treat::Entities
     attr_accessor :type
     
     # Autoload all the entity classes.
-    include Treat::Module::Autoloaded
+    include Treat::Autoloaded
     
     # Implements support for #register,
     # #registry, and #contains_* methods.
