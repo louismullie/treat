a55cff3460dc4d8a750f9b9233265ff07e006792
Louis Mullie <louis.mullie@gmail.com>
2012-09-30 01:17:50 -0400

    Got this backwards, fixing it.

--- lib/treat/config/config.rb
+++ lib/treat/config/config.rb
@@ -77,7 +77,7 @@ module Treat::Config
   
   # Map all classes in Treat::Entities to
   # a global builder function (Entity, etc.)
-  def self.sweeten_entities(off = false)
+  def self.sweeten_entities(on = true)
     Treat.core.entities.list.each do |type|
       next if type == :Symbol
       kname = cc(type).intern
@@ -85,21 +85,21 @@ module Treat::Config
       Object.class_eval do
         define_method(kname) do |val, opts={}|
           klass.build(val, opts)
-        end if !off
-        remove_method(name) if off 
+        end if on
+        remove_method(name) if !on
       end
     end
   end
   
   # Map all classes in the Learning module
   # to a global builder function (e.g. DataSet).
-  def self.sweeten_learning(off = false)
+  def self.sweeten_learning(on = true)
     Treat::Learning.constants.each do |kname|
       Object.class_eval do
         define_method(kname) do |*args| 
           Treat::Learning.const_get(kname).new(*args)
-        end if !off
-        remove_method(name) if off
+        end if on
+        remove_method(name) if !on
       end
     end
   end
