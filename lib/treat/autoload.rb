3b90a394a8ff62d69655d468ddf7fe1ae04fa925
Louis Mullie <louis.mullie@gmail.com>
2012-09-29 23:26:59 -0400

    Slight refactoring to make more concise; added comments.

--- lib/treat/autoload.rb
+++ lib/treat/autoload.rb
@@ -1,3 +1,6 @@
+# Basic mixin for all the main modules,
+# which takes care of requiring the right
+# files in the right order for each one.
 module Treat::Autoload
   
   # Loads all the files for the base
@@ -5,26 +8,17 @@ module Treat::Autoload
   def self.included(base)
     # Get the parts of module name.
     bits = base.to_s.split('::')
-    # Get the module's name itself.
-    name = bits[-1].downcase
     # Singularize the module name.
-    n = self.singularize(n) + '.rb'
-    d = File.join(Dir.pwd, 'lib/treat',
-    bits[1..-1].collect! { |ch| ch.
-    downcase }.join('/')) + '/'
-    # Require base class if present.
+    w = bits[-1].downcase
+    n = (w[-3..-1] == 'ies' ? (w[0..-4] + 'y') :
+    (w[-1] == 's' ? w[0...-1] : w)) + '.rb'
+    # Get the module's directory.
+    path = bits.join('/').downcase
+    d = Dir.pwd + '/lib/' + path + '/'
+    # Require base class if exists.
     require d + n if File.readable?(d + n)
     # Require all other files in dir.
     Dir.glob("#{d}*.rb").each { |f| require f }
-  end
-
-  # Very naive singularization.
-  # If ends in -ies, return -y form.
-  # If ends in -s, return form w/o s.
-  # Otherwise, return the original.
-  def self.singularize(word)
-    (w[-3..-1] == 'ies' ? (w[0..-4] + 'y') :
-    (w[-1] == 's' ? w[0...-1] : w))
   end
   
 end
