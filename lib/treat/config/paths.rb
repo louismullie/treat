deda96e7cc78e45dbb16770e3b3cf142cfc420a5
Louis Mullie <louis.mullie@gmail.com>
2012-10-20 16:06:07 -0400

    Isolate config logic inside classes.

--- lib/treat/config/paths.rb
+++ lib/treat/config/paths.rb
@@ -1,20 +1,19 @@
-module Treat::Config::Paths
-  
-  # * Helper methods for path config * #
+class Treat::Config::Paths
   
   # Get the path configuration based on the 
   # directory structure loaded into Paths.
-  def self.get_paths_config
-    Hash[
+  def self.configure!
+    super
+    root = File.dirname(File.expand_path(         # FIXME
+    __FILE__)).split('/')[0..-4].join('/') + '/'
+    self.config = Hash[
     # Get a list of directories in treat/
-    Dir.glob(self.get_path + '*').select do |path|
+    Dir.glob(root + '*').select do |path|
       FileTest.directory?(path)
     # Map to pairs of [:name, path]
     end.map do |path|
       [File.basename(path).intern, path + '/']
     end]
   end
-  
-  config[:paths] = self.get_paths_config
   
 end
