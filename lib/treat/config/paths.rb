544a2a115e5718bbacb7b4ea52c18c4f9e901cea
Louis Mullie <louis.mullie@gmail.com>
2012-10-21 16:39:54 -0400

    Paths configuration does not need super.

--- lib/treat/config/paths.rb
+++ lib/treat/config/paths.rb
@@ -2,8 +2,9 @@ class Treat::Config::Paths
   
   # Get the path configuration based on the 
   # directory structure loaded into Paths.
+  # Note that this doesn't call super, as
+  # there is no external config files to load.
   def self.configure!
-    super
     root = File.dirname(File.expand_path(         # FIXME
     __FILE__)).split('/')[0..-4].join('/') + '/'
     self.config = Hash[
