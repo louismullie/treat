06913afa8a88f45e7865f492ea8f7df11b472304
Louis Mullie <louis.mullie@gmail.com>
2012-10-21 16:41:14 -0400

    Add to_struct method to Hash helper.

--- lib/treat/helpers/hash.rb
+++ lib/treat/helpers/hash.rb
@@ -1,18 +1,29 @@
+# Helper methods to manipulate hashes.
 class Treat::Helpers::Hash
 
-  # Allow getting the caller method in any context.
-  Hash.class_eval do
+  # Mixin to allow conversion of hashes to
+  # nested structs with the keys as attributes.
+  module ToStruct
     # Converts a hash to nested structs.
-    def self.hash_to_struct(hash)
-      return hash if hash.keys.
-      select { |k| !k.is_a?(Symbol) }.size > 0
-      struct = Struct.new(*hash.keys).new(*hash.values)
+    def to_struct
+      hash = self
+      symbols = hash.keys.select { |k| 
+      !k.is_a?(Symbol) }.size
+      return hash if symbols > 0
+      klass = Struct.new(*hash.keys)
+      struct = klass.new(*hash.values)
       hash.each do |key, value|
         if value.is_a?(Hash)
-          struct[key] = self.hash_to_struct(value)
+          v = value.to_struct
+          struct[key] = v
         end
       end; return struct
     end
+  end
+  
+  # Include the mixins on the core Hash class.
+  Hash.class_eval do
+    include Treat::Helpers::Hash::ToStruct
   end
 
 end
