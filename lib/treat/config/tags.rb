7a902900b2ec073cec3c67bf281ad9902cc4171b
Louis Mullie <louis.mullie@gmail.com>
2012-10-21 17:00:17 -0400

    Improve documentation.

--- lib/treat/config/tags.rb
+++ lib/treat/config/tags.rb
@@ -1,6 +1,8 @@
+# Handles all configuration related 
+# to understanding of part of speech
+# and phrasal tags.
 class Treat::Config::Tags
   
-  # Load and align tags.
   # Generate a map of word and phrase tags 
   # to their syntactic category, keyed by 
   # tag set.
@@ -16,21 +18,20 @@ class Treat::Config::Tags
     align_tags(phrase_tags, tag_sets)
     self.config[:aligned] = config
  end
-  
-  # * Helper methods for tag set config * #
-  
-   # Align tag tags in the tag set 
-   def self.align_tags(tags, tag_sets)
-     wttc = {}
-     tags.each_slice(2) do |desc, tags|
-       category = desc.gsub(',', ' ,').
-       split(' ')[0].downcase
-       tag_sets.each_with_index do |tag_set, i|
-         next unless tags[i]
-         wttc[tags[i]] ||= {}
-         wttc[tags[i]][tag_set] = category
-       end
-     end; return wttc
-   end
+ 
+ # Helper methods for tag set config.
+ # Align tag tags in the tag set 
+ def self.align_tags(tags, tag_sets)
+   wttc = {}
+   tags.each_slice(2) do |desc, tags|
+     category = desc.gsub(',', ' ,').
+     split(' ')[0].downcase
+     tag_sets.each_with_index do |tag_set, i|
+       next unless tags[i]
+       wttc[tags[i]] ||= {}
+       wttc[tags[i]][tag_set] = category
+     end
+   end; return wttc
+ end
    
 end
