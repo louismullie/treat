61d683603ca4985f48781da0574f71b8cea9ff51
Louis Mullie <louis.mullie@gmail.com>
2012-10-24 01:28:00 -0400

    Finalize the implementation of MLPs with FANN.

--- lib/treat/workers/learners/classifiers/mlp.rb
+++ lib/treat/workers/learners/classifiers/mlp.rb
@@ -1,30 +1,39 @@
 # Classification based on a multilayer perceptron.
 class Treat::Workers::Learners::Classifiers::MLP
   
-  require 'ai4r'
+  require 'ruby_fann/neural_network'
   
-  @@mlps = {}
+  DefaultOptions = {
+    num_inputs: 3,
+    hidden_neurons: [2, 8, 4, 3, 4],
+    num_outputs: 1,
+    max_neurons: 1000,
+    neurons_between_reports: 1,
+    desired_error: 0.1
+  }
+  
+  @@classifiers = {}
   
   def self.classify(entity, options = {})
-    
-    set = options[:training]
-    cl = set.problem
-      
-    if !@@mlps[cl]
-      net = Ai4r::NeuralNetwork::Backpropagation.new(
-      [cl.feature_labels.size, 3, 1])
-      set.items.each do |item|
-        inputs = item[:features][0..-2]
-        outputs = [item[:features][-1]]
-        net.train(inputs, outputs)
-      end
-      @@mlps[cl] = net
+    options = DefaultOptions.merge(options)
+    dset = options[:training]
+    prob, items = dset.problem, dset.items
+    if !@@classifiers[prob]
+      fann = RubyFann::Standard.new(options)
+      inputs = items.map { |it| it[:features][0..-2] }
+      outputs = items.map { |it| [it[:features][-1]] }
+      training = RubyFann::TrainData.new(
+      inputs: inputs, desired_outputs: outputs)
+      params = [options[:max_neurons],
+      options[:neurons_between_reports],
+      options[:desired_error]]
+      fann.train_on_data(training, *params)
+      @@classifiers[prob] = fann
     else
-      net = @@mlps[cl]
+      fann = @@classifiers[prob]
     end
-    
-    net.eval(cl.export_features(entity, false))[0]
-    
+    vect = prob.export_features(entity, false)
+    fann.run(vect)[0]
   end
   
 end
