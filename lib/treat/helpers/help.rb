5c1ca9d9f128add53f3e312335756729feac397c
Louis Mullie <louis.mullie@gmail.com>
2012-10-28 01:34:26 -0400

    Move Levensthein to a similarity annotator.

--- lib/treat/helpers/help.rb
+++ lib/treat/helpers/help.rb
@@ -32,30 +32,4 @@ class Treat::Helpers::Help
     msg
   end
   
-  # Return the levensthein distance between
-  # two strings taking into account the costs
-  # of insertion, deletion, and substitution.
-  # Used by did_you_mean? to detect typos.
-  def self.levenshtein(first, other, ins=1, del=1, sub=1)
-    return nil if first.nil? || other.nil?
-    dm = []
-    dm[0] = (0..first.length).collect { |i| i * ins}
-    fill = [0] * (first.length - 1).abs
-    for i in 1..other.length
-      dm[i] = [i * del, fill.flatten]
-    end
-    for i in 1..other.length
-      for j in 1..first.length
-        dm[i][j] = [
-          dm[i-1][j-1] +
-          (first[i-1] ==
-          other[i-1] ? 0 : sub),
-          dm[i][j-1] + ins,
-          dm[i-1][j] + del
-        ].min
-      end
-    end
-    dm[other.length][first.length]
-  end
-
 end
