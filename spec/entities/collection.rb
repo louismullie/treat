a7b4a7eea81a9bb11b8e6feab92e832865982692
Louis Mullie <louis.mullie@gmail.com>
2012-12-11 19:22:59 -0500

    Fix specs in accordance with 6c5f31fd5e19d45f6e7f5f6393a02459b01eabb9.

--- spec/entities/collection.rb
+++ spec/entities/collection.rb
@@ -15,7 +15,7 @@ describe Treat::Entities::Collection do
         it "recursively searches the folder for " +
         "files and opens them into a collection of documents" do
           collection = Treat::Entities::Collection.build(@file)
-          collection.size.should eql 5
+          collection.size.should eql 6
         end
 
       end
@@ -40,7 +40,7 @@ describe Treat::Entities::Collection do
         f = Treat.paths.spec + 'workers/examples/english/economist'
         c = Treat::Entities::Collection.build(f)
         c << Treat::Entities::Document.new
-        c.size.should eql 3
+        c.size.should eql 4
       end
 
   end
